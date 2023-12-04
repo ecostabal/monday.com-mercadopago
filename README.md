122e8257f9c7d92343764e84cde8c4577216de9c
ecostabal <ecostabal@urbex.cl>
2023-12-04 19:14:24 -0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Integracion Monday.com - MercadoPago Checkout Pro
+Integracion Monday.com - Flow
 
 La integración es un webhook que genera un link de cobro una vez que cambia el estado en monday.
--- function.js
+++ function.js
@@ -1,13 +1,28 @@
-const { MercadoPagoConfig, Payment } = require('mercadopago');
 const axios = require('axios');
+const CryptoJS = require('crypto-js');
 
-// Inicializa el objeto cliente de MercadoPago
-const client = new MercadoPagoConfig({
-    accessToken: 'APP_USR-2324171826250686-120122-c073e6b1f6946e5425eafc25895f7de6-304962566'
-});
+// Configuración de Flow
+const flowConfig = {
+    apiKey: '26FF859D-5E21-4E57-9BBD-7D0BAD8L06CC', // Reemplaza con tu API Key de Flow
+    secretKey: '3756483e4e45238c83a7fb6112a4aba948f83728', // Reemplaza con tu Secret Key de Flow
+    // ...otros parámetros de configuración...
+};
 
-// Función para manejar webhooks de Monday.com y generar link de pago
-exports.generarLinkPago = async (req, res) => {
+// Función para firmar los parámetros con secretKey
+const firmarParametros = (parametros, secretKey) => {
+    const stringToSign = Object.keys(parametros)
+        .sort()
+        .map(key => key + parametros[key])
+        .join('');
+
+    // Utiliza el algoritmo SHA-256 y tu secretKey para firmar
+    const signature = CryptoJS.HmacSHA256(stringToSign, secretKey).toString(CryptoJS.enc.Hex);
+
+    return signature;
+};
+
+// Función para manejar webhooks de Monday.com y generar link de pago con Flow
+exports.generarLinkPagoFlow = async (req, res) => {
     try {
         console.log("Inicio de la función");
 
@@ -32,7 +47,7 @@ exports.generarLinkPago = async (req, res) => {
             query: query
         }, {
             headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE',
+                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE',
                 'Content-Type': 'application/json'
             }
         });
@@ -40,7 +55,6 @@ exports.generarLinkPago = async (req, res) => {
         console.log("Respuesta de Monday.com:", mondayResponse.data);
 
         const columnsData = mondayResponse.data.data.items[0].column_values;
-
         const montoColumn = columnsData.find(column => column.id === 'n_meros03');
         const descripcionColumn = columnsData.find(column => column.id === 'ubicaci_n');
         const emailColumn = columnsData.find(column => column.id === 'correo_electr_nico');
@@ -49,68 +63,61 @@ exports.generarLinkPago = async (req, res) => {
             throw new Error('Datos necesarios no están presentes en el evento');
         }
 
-        // Obtienes el monto y lo conviertes a número flotante
         const monto = parseFloat(montoColumn.text);
-
-        // Aquí verificas si monto es un número válido
-        if (monto === null || isNaN(monto)) {
+        if (isNaN(monto)) {
             throw new Error('El monto no es un número válido');
         }
 
         const descripcion = descripcionColumn.text;
         const email = emailColumn.text;
 
-        console.log(monto)
-        console.log(descripcion)
-        console.log(email)
+        // Creación de la orden de pago para Flow
+        const ordenCobro = {
+            apiKey: flowConfig.apiKey,
+            subject: descripcion,
+            currency: 'CLP',
+            amount: monto,
+            email: email,
+            paymentMethod: 9,
 
-        // Inicializa el objeto API de pago
-        const payment = new Payment(client);
-        const montoFinal = monto * 1; // Calcula el monto total
-        
-        const preference = {
-            items: [{
-                title: descripcion,
-                quantity: 1,
-                currency_id: 'CLP',
-                unit_price: monto,
-            }],
-            payer: { email: email },
-            payment_methods: {
-                excluded_payment_methods: [],
-                excluded_payment_types: [],
-                installments: 6, // Ejemplo: Número de cuotas
-            },
-            transaction_amount: parseFloat(montoFinal), // Campo en el nivel correcto
-            // Otros campos y configuraciones necesarios...
         };
-        
-        console.log(preference)
-        console.log(montoFinal)
-        
-        
-        // Realiza la solicitud para crear la preferencia de pago
-        let preferenceResponse = await payment.create(preference);
-        let preferenceId = preferenceResponse.body.id;
-        console.log(preferenceResponse)
 
-        // Construir el enlace de MercadoPago utilizando el preferenceId
-        const linkDePago = `https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=${preferenceId}`;
+        // Firma de los parámetros con tu secretKey de Flow
+        ordenCobro.s = firmarParametros(ordenCobro, flowConfig.secretKey);
 
+
+        // Envío de la orden de pago a Flow
+        let flowResponse = await axios.post('https://www.flow.cl/api/payment/create', ordenCobro, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                // Aquí debes incluir cualquier otra cabecera necesaria, como autenticación
+            }
+        });
+
+        if (!flowResponse.data || !flowResponse.data.token) {
+            throw new Error('No se pudo obtener el token de pago de Flow');
+        }
+
+        const token = flowResponse.data.token;
+
+        // Construir la URL de redirección
+        const urlRedireccion = `sandbox.flow.cl/app/web/pay.php?token=${token}`; // Reemplaza con tu URL de redirección
+
+        // Actualizar el enlace en Monday.com
         await axios.post('https://api.monday.com/v2/', {
             query: `mutation { 
-                change_column_value (board_id: 5598495616, item_id: ${itemId}, column_id: "enlace", value: "{\"url\": \"${linkDePago}\"}") { 
+                change_column_value (board_id: 5598495616, item_id: ${itemId}, column_id: "enlace", value: "{\"url\": \"${urlRedireccion}\"}") { 
                     id 
                 } 
             }`
         }, {
             headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE',
+                'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzMjg3MzUyNCwiYWFpIjoxMSwidWlkIjoyMzUzNzM2NCwiaWFkIjoiMjAyMy0wMS0zMVQyMTowMjoxNy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUwNzUxNiwicmduIjoidXNlMSJ9.lX1RYu90B2JcH0QxITaF8ymd4d6dBes0FJHPI1mzSRE',
                 'Content-Type': 'application/json'
             }
         });
 
-        res.json({ mensaje: "Link de pago generado y actualizado en Monday.com", linkDePago: linkDePago });
+        res.json({ mensaje: "Link de pago generado y actualizado en Monday.com", linkDePago: urlRedireccion });
     } catch (error) {
         console.error('Error capturado en la función:', error);
         res.status(500).json({ mensaje: "Error en la función", error: error.message, stack: error.stack });
