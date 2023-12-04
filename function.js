4d9166b5fe4ce29f973b08d9914af5c33b11244c
ecostabal <ecostabal@urbex.cl>
2023-12-04 03:04:15 -0300

    changes

--- function.js
+++ function.js
@@ -1,30 +1,47 @@
 const { MercadoPagoConfig, Payment } = require('mercadopago');
 const axios = require('axios');
 
-// Configuración de MercadoPago con token de prueba
+// Inicializa el objeto cliente de MercadoPago
 const client = new MercadoPagoConfig({
-    accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN
+    accessToken: MERCADOPAGO_ACCESS_TOKEN
 });
-const payment = new Payment(client);
 
 // Función para manejar webhooks de Monday.com y generar link de pago
 exports.generarLinkPago = async (req, res) => {
     try {
         console.log("Inicio de la función");
 
-        // Agrega el registro de la solicitud recibida
-        console.log("Solicitud recibida:", JSON.stringify(req.body, null, 2));
-        
-        // Verificar la estructura de la solicitud
-        if (!req.body || !req.body.event || !req.body.event.pulse || !req.body.event.pulse.column_values) {
+        if (!req.body || !req.body.event || !req.body.event.pulseId) {
             throw new Error('La solicitud no contiene la estructura esperada de un evento de Monday.com');
         }
 
-        const eventData = req.body.event;
-        const columnsData = eventData.pulse.column_values;
+        const itemId = req.body.event.pulseId;
 
-        // Obtener los valores de las columnas por ID de columna
-        const montoColumn = columnsData.find(column => column.id === 'f_rmula0');
+        const query = `query {
+            items(ids: [${itemId}]) {
+                column_values {
+                    id
+                    type
+                    value
+                    text
+                }
+            }
+        }`;
+
+        let mondayResponse = await axios.post('https://api.monday.com/v2', {
+            query: query
+        }, {
+            headers: {
+                'Authorization': MONDAY_API_TOKEN,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log("Respuesta de Monday.com:", mondayResponse.data);
+
+        const columnsData = mondayResponse.data.data.items[0].column_values;
+
+        const montoColumn = columnsData.find(column => column.id === 'n_meros03');
         const descripcionColumn = columnsData.find(column => column.id === 'ubicaci_n');
         const emailColumn = columnsData.find(column => column.id === 'correo_electr_nico');
 
@@ -32,45 +49,60 @@ exports.generarLinkPago = async (req, res) => {
             throw new Error('Datos necesarios no están presentes en el evento');
         }
 
-        // Convertir el valor de la columna "Monto" a número
         const monto = parseFloat(montoColumn.text);
         const descripcion = descripcionColumn.text;
         const email = emailColumn.text;
 
-        console.log("Monto:", monto, "Descripción:", descripcion, "Email:", email);
+        console.log(monto)
+        console.log(descripcion)
+        console.log(email)
 
-        // Crear el link de pago
-        const paymentResponse = await payment.create({
-            body: {
-                transaction_amount: monto,
-                description: descripcion,
-                payer: {
-                    email: email
-                }
-            }
-        });
+        // Inicializa el objeto API de pago
+        const payment = new Payment(client);
+        const montoFinal = monto * 1; // Calcula el monto total
 
-        console.log("Respuesta de Mercado Pago:", paymentResponse);
+        const preference = {
+            items: [{
+                title: descripcion,
+                quantity: 1,
+                currency_id: 'CLP',
+                unit_price: monto,
+            }],
+            payer: { email: email },
+            payment_methods: {
+                excluded_payment_methods: [],
+                excluded_payment_types: [],
+                installments: 6, // Ejemplo: Número de cuotas
+            transaction_amount: montoFinal, // Utiliza la variable montoFinal
 
-        const linkDePago = paymentResponse.response.point_of_interaction.transaction_data.ticket_url;
-        console.log("Link de pago:", linkDePago);
+            },
+            // Otros campos y configuraciones necesarios...
+        };
+        console.log(preference)
+        console.log(montoFinal)
+        
+        // Realiza la solicitud para crear la preferencia de pago
+        let preferenceResponse = await payment.create(preference);
+        let preferenceId = preferenceResponse.body.id;
+        console.log(preferenceResponse)
 
-        // Actualizar la columna de enlace en Monday.com con el link de pago
-        const updateResponse = await axios.post('https://api.monday.com/v2/', {
+        // Construir el enlace de MercadoPago utilizando el preferenceId
+        const linkDePago = `https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=${preferenceId}`;
+
+        await axios.post('https://api.monday.com/v2/', {
             query: `mutation { 
-                change_column_value (board_id: 5598495616, item_id: ${eventData.pulse.id}, column_id: "enlace", value: "{\"url\": \"${linkDePago}\"}") { 
+                change_column_value (board_id: 5598495616, item_id: ${itemId}, column_id: "enlace", value: "{\"url\": \"${linkDePago}\"}") { 
                     id 
                 } 
             }`
         }, {
             headers: {
-                'Authorization': process.env.MONDAY_API_TOKEN
+                'Authorization': MONDAY_API_TOKEN,
+                'Content-Type': 'application/json'
             }
         });
 
-        console.log("Respuesta de la actualización en Monday.com:", updateResponse);
-
-        res.json({ mensaje: "Link de pago generado y actualizado en Monday.com" });
+        res.json({ mensaje: "Link de pago generado y actualizado en Monday.com", linkDePago: linkDePago });
     } catch (error) {
         console.error('Error capturado en la función:', error);
         res.status(500).json({ mensaje: "Error en la función", error: error.message, stack: error.stack });
